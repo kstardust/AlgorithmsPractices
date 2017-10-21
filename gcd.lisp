ee53c7d830707ff79ea76a2941022a8c57dc5e0c
Kevin <lql9705@gmail.com>
2017-10-21 21:38:43 +0800

    gcd

---
+++ gcd.lisp
@@ -0,0 +1,14 @@
+(defun is-gcd(x y)
+    (if (equalp y 0)
+	x
+	(is-gcd y (mod x y))))
+
+(defun ext-euclid(x y)
+  (if (equalp y 0)
+      (list :a 1 :b 0 :g x)
+      (let ((res (ext-euclid y (mod x y))))
+	(list :a (getf res :b)
+	      :b (- (getf res :a) (* (getf res :b) (floor x y)))
+	      :g (getf res :g)))))
+
+(format t "GCD ~a~%" (ext-euclid 796 90))
